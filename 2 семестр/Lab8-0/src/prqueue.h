0a6548acfa697fadbf82936629422a487c969cfb
Ivan <java.enjoyer@gmail.com>
2021-05-28 17:31:03 +0500

    Create prqueue.h

---
+++ 2 семестр/Lab8-0/src/prqueue.h
@@ -0,0 +1,21 @@
+#ifndef LAB8_0_PRQUEUE_H
+#define LAB8_0_PRQUEUE_H
+
+typedef struct heapItem{
+    short vertex;
+    long long priority;
+}HeapItem;
+
+typedef struct heap{
+    short size;
+    HeapItem* items;
+}Heap;
+
+Heap* createHeap(short n);
+void freeHeap(Heap* h);
+void heapSwap(HeapItem* a, HeapItem* b);
+void insert(Heap* h);
+short extractMin(Heap* h);
+void decreaseKey(Heap* h, int vertex, int np);
+
+#endif
