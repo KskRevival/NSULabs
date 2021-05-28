683b6fd1371eb83b35e065598e9265d4bab2b7a5
Ivan <java.enjoyer@gmail.com>
2021-05-28 17:21:40 +0500

    Create qsort.h

---
+++ 2 семестр/Lab8-1/qsort.h
@@ -0,0 +1,8 @@
+#ifndef LAB7_QSORT_H
+#define LAB7_QSORT_H
+
+void swap(int * a, int * b);
+int partition (int* u, int* v, int* len, int l, int r);
+void quickSort (int* u, int* v, int* len, int l, int r);
+
+#endif //LAB7_QSORT_H
